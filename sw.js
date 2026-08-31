36dd93aae447865a61209531cb76eb32ce2b0674
Nikoleta Wellness Coach <nikol_papadaki@yahoo.com>
2026-08-31 23:09:10 +0300

    Cache the upgraded Wellness CEO app for offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="nwceo-v7";
-const FILES=["./","./index.html","./styles.css","./app-v3.js","./documents-addon.js","./manifest.json"];
+const CACHE="nwceo-v8";
+const FILES=["./","./index.html","./styles.css","./migrate-v5.js","./app-v3.js","./documents-addon.js","./manifest.json"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(cached=>{const network=fetch(e.request).then(r=>{if(r.ok&&new URL(e.request.url).origin===location.origin)caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>cached);return cached||network}))});
