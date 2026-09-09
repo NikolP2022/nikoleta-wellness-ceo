c52d2a6ebf77a5d31523c9fe4d71e57c7759f1b3
Nikoleta Wellness Coach <nikol_papadaki@yahoo.com>
2026-09-09 17:58:45 +0300

    Add network-first service worker v230

---
+++ sw-v230.js
@@ -0,0 +1,4 @@
+const CACHE='nwceo-v230';
+self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{if(r.ok){const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));}return r}).catch(()=>caches.match(e.request)))});
